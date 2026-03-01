b925af2407665a23ddcd862340c436d29d996646
h1zz <pavelrog315@gmail.com>
2026-03-01 11:35:25 +0300

    Update and rename main.py to parser.py

--- source/parser.py
+++ source/parser.py
@@ -25,7 +25,33 @@ ALLOWED_PROTOCOLS = ['vless', 'vmess', 'ss']
 
 #ИСТОЧНИКИ
 SOURCES = [
-    "https://raw.githubusercontent.com/barry-far/V2ray-Config/refs/heads/main/All_Configs_Sub.txt",
+    "https://github.com/sakha1370/OpenRay/raw/refs/heads/main/output/all_valid_proxies.txt", 
+    "https://raw.githubusercontent.com/sevcator/5ubscrpt10n/main/protocols/vl.txt", 
+    "https://raw.githubusercontent.com/yitong2333/proxy-minging/refs/heads/main/v2ray.txt", 
+    "https://raw.githubusercontent.com/acymz/AutoVPN/refs/heads/main/data/V2.txt", 
+    "https://raw.githubusercontent.com/miladtahanian/V2RayCFGDumper/refs/heads/main/sub.txt", 
+    "https://raw.githubusercontent.com/roosterkid/openproxylist/main/V2RAY_RAW.txt", 
+    "https://github.com/Epodonios/v2ray-configs/raw/main/Splitted-By-Protocol/trojan.txt", 
+    "https://raw.githubusercontent.com/CidVpn/cid-vpn-config/refs/heads/main/general.txt", 
+    "https://raw.githubusercontent.com/mohamadfg-dev/telegram-v2ray-configs-collector/refs/heads/main/category/vless.txt", #9
+    "https://raw.githubusercontent.com/mheidari98/.proxy/refs/heads/main/vless", 
+    "https://raw.githubusercontent.com/youfoundamin/V2rayCollector/main/mixed_iran.txt", 
+    "https://github.com/expressalaki/ExpressVPN/blob/main/configs3.txt", 
+    "https://raw.githubusercontent.com/MahsaNetConfigTopic/config/refs/heads/main/xray_final.txt", 
+    "https://github.com/LalatinaHub/Mineral/raw/refs/heads/master/result/nodes", 
+    "https://github.com/miladtahanian/Config-Collector/raw/refs/heads/main/vless_iran.txt", 
+    "https://raw.githubusercontent.com/Pawdroid/Free-servers/refs/heads/main/sub", 
+    "https://github.com/MhdiTaheri/V2rayCollector_Py/raw/refs/heads/main/sub/Mix/mix.txt", 
+    "https://raw.githubusercontent.com/free18/v2ray/refs/heads/main/v.txt", 
+    "https://github.com/MhdiTaheri/V2rayCollector/raw/refs/heads/main/sub/mix", 
+    "https://github.com/Argh94/Proxy-List/raw/refs/heads/main/All_Config.txt", 
+    "https://raw.githubusercontent.com/shabane/kamaji/master/hub/merged.txt", 
+    "https://raw.githubusercontent.com/wuqb2i4f/xray-config-toolkit/main/output/base64/mix-uri", 
+    "https://raw.githubusercontent.com/Delta-Kronecker/V2ray-Config/refs/heads/main/config/protocols/vless.txt", 
+    "https://raw.githubusercontent.com/STR97/STRUGOV/refs/heads/main/STR.BYPASS#STR.BYPASS%F0%9F%91%BE", 
+    "https://raw.githubusercontent.com/V2RayRoot/V2RayConfig/refs/heads/main/Config/vless.txt",
+    "https://raw.githubusercontent.com/barry-far/V2ray-Config/refs/heads/main/All_Configs_Sub.txt"
+
 
 ]
 
@@ -351,4 +377,5 @@ async def main():
 
 # -------------------- ЗАПУСК --------------------
 if __name__ == "__main__":
+
     asyncio.run(main())
